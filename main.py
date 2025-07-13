eec1519012f92f54cd4ed4d00d6c0ecc9e1af715
Prateek Swami <prateek.swami2004@gmail.com>
2025-07-13 14:51:12 +0530

    Changes done

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # ✅ Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your frontend URL
+    allow_origins=["https://prateek2384.github.io"],  # Your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
